d071a9fc2cce0bba8871e9b864c2742c628c642f
Sempakonka <47318358+Sempakonka@users.noreply.github.com>
2021-02-26 17:32:48 +0100

    beginnings of being able to reflect on day

--- lib/reflectDay.dart
+++ lib/reflectDay.dart
@@ -1,18 +1,21 @@
-import 'package:flutter/material.dart';
 import 'package:dailygoals_app/Reflect.dart';
+import 'package:dailygoals_app/globals.dart' as globals;
+import 'package:flutter/material.dart';
+import 'package:jiffy/jiffy.dart';
 
 class reflectDay extends StatefulWidget {
   static const routeName = "/reflectDayState";
+
   @override
   _reflectDayState createState() => _reflectDayState();
 }
 
 class _reflectDayState extends State<reflectDay> {
   DateTime selectedDay;
+
   @override
   Widget build(BuildContext context) {
-
-   selectedDay = ModalRoute.of(context).settings.arguments;
+    selectedDay = ModalRoute.of(context).settings.arguments;
     return Scaffold(
       appBar: AppBar(
         elevation: 0,
@@ -25,10 +28,103 @@ class _reflectDayState extends State<reflectDay> {
           },
         ),
       ),
-
       body: Center(
-        child: Text(selectedDay.toString()),
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          //Center Column contents vertically,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          //Center Column contents horizontally,
+          children: [
+            Text(
+              () {
+                String theReflectingDay =
+                    Jiffy(selectedDay).format("EEEE MMM do ");
+                return "You're reflecting on $theReflectingDay";
+              }(),
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20,
+                  color: Theme.of(context).primaryColor,
+                  fontWeight: FontWeight.bold),
+            ),
+            Text(
+              "So, did you reach your goals?",
+              textAlign: TextAlign.center,
+              style: TextStyle(
+                  fontSize: 20, color: Theme.of(context).primaryColor),
+            ),
+            Expanded(
+              child: ListView.builder(
+                  itemCount: globals.activatedDays[selectedDay].goals.length,
+                  itemBuilder: (BuildContext context, int index) =>
+                      buildGoalsList(context, index, selectedDay)),
+            )
+          ],
+        ),
       ),
     );
+  }
+
+  Widget buildGoalsList(
+      BuildContext context, int index, DateTime _selectedDay) {
+    return new Container(
+        margin: EdgeInsets.only(left: 9, top: 0, right: 9, bottom: 12),
+        decoration: BoxDecoration(
+            color: Colors.white,
+            borderRadius: BorderRadius.only(
+                topLeft: Radius.circular(10),
+                topRight: Radius.circular(10),
+                bottomLeft: Radius.circular(10),
+                bottomRight: Radius.circular(10)),
+            boxShadow: [
+              BoxShadow(
+                color: Colors.grey.withOpacity(0.5),
+                spreadRadius: 1,
+                blurRadius: 2,
+                offset: Offset(0, 2),
+              )
+            ]),
+        child: ListTile(
+          onTap: () {
+            ///TODO: expand to see the description, if no description than expand to show "no description"
+          },
+          title: Center(
+            child: Column(children: [
+              Text(globals.activatedDays[_selectedDay].goals[index].title, style: TextStyle(fontSize: 18),),
+              SizedBox(
+                child: Row(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  //Center Row contents horizontally,
+                  crossAxisAlignment: CrossAxisAlignment.center,
+                  //Center Row contents vertically,
+
+                  children: [
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.check,
+                        color: globals.greenColor,
+                      ),
+                      color: Colors.white,
+                      shape: CircleBorder(
+                        side: BorderSide(color: globals.greenColor),
+                      ),
+                    ),
+                    RaisedButton(
+                      onPressed: () {},
+                      child: Icon(
+                        Icons.clear,
+                        color: globals.redColor,
+                      ),
+                      color: Colors.white,
+                      shape: CircleBorder(
+                          side: BorderSide(color: globals.redColor)),
+                    ),
+                  ],
+                ),
+              ),
+            ]),
+          ),
+        ));
   }
 }
